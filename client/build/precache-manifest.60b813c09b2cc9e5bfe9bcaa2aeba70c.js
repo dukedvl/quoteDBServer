836bbef0076899ca51b4d780012ff4f8e3a88c15
Jeff <dukedvl@gmail.com>
2022-10-12 23:53:31 -0500

    some audit fixes

---
+++ client/build/precache-manifest.60b813c09b2cc9e5bfe9bcaa2aeba70c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bea77d42943c74c59630dc54d89dee1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f55269678caed1de61a",
+    "url": "/static/css/main.352f3be4.chunk.css"
+  },
+  {
+    "revision": "d2cc1e8098001a4ee070",
+    "url": "/static/js/2.853369b2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.853369b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f55269678caed1de61a",
+    "url": "/static/js/main.da2a70d6.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
